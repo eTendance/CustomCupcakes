d7e7b0d781cf88d99eab940f91c8537d8f53e01c
jiaxinli <jiaxinli@smu.edu>
2013-10-11 13:32:46 -0500

    order2.css

---
+++ GUI-pages/order2.cc
@@ -0,0 +1,143 @@
+#order {
+	float:right;
+	width: 216px;
+  	height: 245px;
+  	background-color: yellow;
+  	border-style: solid;
+}
+
+#order p{
+	width: 200px;
+  	height: 200px;
+  	background-color: white;
+  	border-style: solid;
+  	overflow:scroll;
+  	text-align:center;
+  	margin-top: 5px;
+	float:right;
+  	margin-right: 5px;
+}
+
+#order_button{
+	float:right;
+	margin-top: -10px;
+	width:60px;
+}
+
+#cost {
+	float:left;
+	margin-top: -22px;
+	width:160px;
+}
+
+#favorite {
+	float:left;
+	width: 160px;
+  	height: 600px;
+  	background-color: yellow;
+  	border-style: solid;
+  	text-align:center;
+  	font-size: 20px;
+}
+
+#favorite div{
+	float:left;
+	width: 140px;
+  	height: 570px;
+  	background-color: white;
+  	border-style: dashed;
+  	margin-left: 7px;
+  	margin-top: 0px; 
+}
+
+#flavor {
+	float:right;
+	width: 700px;
+  	height: 220px;
+  	background-color: yellow;
+  	border-style: solid;
+  	text-align:center;
+  	font-size: 20px;
+  	margin-right: 120px;
+}
+
+#flavor div{
+	float:right;
+	width: 680px;
+  	height: 190px;
+  	background-color: white;
+  	border-style: dotted;
+  	margin-top: -2px; 
+  	margin-right:7px;
+  	overflow:scroll;
+  	border-color: red;
+}
+
+#flavor table{
+}
+
+#filling {
+	float:right;
+	width: 700px;
+  	height: 170px;
+  	background-color: yellow;
+  	border-style: solid;
+  	text-align:center;
+  	font-size: 20px;
+  	margin-right: 120px;
+  	margin-top: 30px;
+}
+
+#filling div{
+	float:right;
+	width: 680px;
+  	height: 140px;
+  	background-color: white;
+  	border-style: dotted;
+  	margin-top: -2px; 
+  	margin-right:7px;
+  	overflow:scroll;
+  	border-color: red;
+}
+
+#icing {
+	float:right;
+	width: 700px;
+  	height: 210px;
+  	background-color: yellow;
+  	border-style: solid;
+  	text-align:center;
+  	font-size: 20px;
+  	margin-right: 342px;
+  	margin-top: 30px;
+}
+
+#icing div{
+	float:right;
+	width: 680px;
+  	height: 180px;
+  	background-color: white;
+  	border-style: dotted;
+  	margin-top: -2px; 
+  	margin-right:7px;
+  	overflow:scroll;
+  	border-color: red;
+}
+
+#toppings {
+	float:right;
+	width: 700px;
+  	height: 160px;
+  	background-color: yellow;
+  	border-style: solid;
+  	text-align:center;
+  	font-size: 20px;
+  	margin-right: 342px;
+  	margin-top: 30px;
+}
+
+footer
+{
+	float:right;
+	clear:both;
+}
